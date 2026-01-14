ab1388f429088dcd6b8784567ce0b2d20f0f93e4
sayantaniy <sayantanimanna139@gmail.com>
2026-01-14 20:39:18 +0530

    access specifiers

---
+++ testing/app.ts
@@ -0,0 +1,23 @@
+import client = require("framer-motion/client");
+
+class BottleMake{
+    private capacity: number;
+    constructor(capacity: number){
+        this.capacity = capacity;
+    }
+}
+
+class Bottle extends BottleMake{
+    constructor(capacity: number){
+        super(capacity);
+    }
+
+    getValue(){
+    console.log(this.capacity) //red comming because private property cannot be accessed but it renders neverthless
+}
+}
+
+
+
+let b1 = new Bottle(100);
+b1.getValue();
